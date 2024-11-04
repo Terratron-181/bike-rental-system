c09b36e0f1868a67a8daa3815fb78771c1c59bd9
Terratron-181 <thickbrowncoffee@gmail.com>
2024-11-04 20:56:09 +0200

    +field user_id to User model and basic functionality to randomise it

--- users.c
+++ users.c
@@ -1,8 +1,65 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 #include "users.h"
+
+
+char *makeID(User *user_instance) {
+
+    char *id_string = malloc(sizeof(char) * 9);// 4 + 4 + 1 ('\0')
+    int *random_number = malloc(sizeof(int));
+    *random_number = 1 + rand() % 10000;
+
+    // filling empty digits with Zeros e.g. 0067
+    char *random_number_string = malloc(sizeof(char) * 5);
+    if ((*random_number >= 1) && (*random_number <= 9)) {
+        sprintf(
+        random_number_string, 
+        "000%d",
+        *random_number
+        );
+    } else if ((*random_number >= 10) && (*random_number <= 99)) {
+        sprintf(
+        random_number_string, 
+        "00%d",
+        *random_number
+        );
+        
+    } else if ((*random_number >= 100) && (*random_number <= 999)) {
+        sprintf(
+        random_number_string, 
+        "0%d",
+        *random_number
+        );   
+    } else if ((*random_number >= 1000) && (*random_number <= 9999)) {
+        sprintf(
+        random_number_string, 
+        "%d",
+        *random_number
+        );
+    };
+
+    random_number_string[4] = '\0';
+    
+    char *first_4_letters = malloc(sizeof(char) * 5);// 4 + 1('\0')
+    strncpy(first_4_letters, user_instance->user_name, 4);
+    first_4_letters[4] = '\0';
+
+    sprintf(
+        id_string, 
+        "%s%s",
+        first_4_letters,
+        random_number_string
+    );
+    
+    printf("\n%s\n", id_string);
+    
+    
+    return id_string;
+
+};
 
 
 int createUser(char *user_name, int *age, int *role, char *Path) {
@@ -12,15 +69,15 @@ int createUser(char *user_name, int *age, int *role, char *Path) {
     strncpy(user_instance->user_name, user_name, (MAX_USERNAME - 1));
     user_instance->user_name[MAX_USERNAME - 1] = '\0';
     user_instance->user_age = *age;
+    strncpy(user_instance->user_id, (makeID(user_instance)), 8);
+    user_instance->user_name[8] = '\0';
 
     
 
     int err_code = formatUser(user_instance, role, Path);
 
-    // free(user_instance);
-    // free(age);
-    // free(role);
-    // free(user_name);
+    
+    free(user_instance);
     return err_code;
 };
 
@@ -37,7 +94,8 @@ int formatUser(User *user_instance, int *role, char *Path) {
     char *formatted_string = malloc(sizeof(User) + 20);
     sprintf(
         formatted_string, 
-        "%s,%d,%s\n", 
+        "%s,%s,%d,%s\n", 
+        user_instance->user_id,
         user_instance->user_name, 
         user_instance->user_age, 
         role_string
@@ -45,7 +103,7 @@ int formatUser(User *user_instance, int *role, char *Path) {
 
     int save_state = saveUser(formatted_string, Path);
 
-    // free(formatted_string);
+    free(formatted_string);
     return save_state;
 };
 
--- users.h
+++ users.h
@@ -13,10 +13,14 @@ typedef struct {
     char user_name[MAX_USERNAME];
     int user_age;
     Role user_role;
+    // So, First 4 letters + a random 4 digits number
+    // 4 + 4 = 8 characters. + 1 character for '\0' = 9 chars.
+    char user_id[9];
 } User;
 
 
 int createUser(char *user_name, int *age, int *role, char *Path);
+char *makeID(User *user_instance);
 int formatUser(User *user_instance, int *role, char *Path);
 int saveUser(char *formatted_user_string, char *Path);
 
